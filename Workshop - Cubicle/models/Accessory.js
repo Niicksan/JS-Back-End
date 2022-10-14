1cc7ab617850d1f6984d80a9b4eb69ca1457af15
NIkolay Koev <nikslotar@abv.bg>
2022-10-14 23:28:57 +0300

    Validate Accessory Model

--- Workshop - Cubicle/models/Accessory.js
+++ Workshop - Cubicle/models/Accessory.js
@@ -1,10 +1,19 @@
 const { Schema, model, Types } = require('mongoose');
 
 
+const URL_REGEX = /^(https?:\/)?\/.*/i;
+
 const accessorySchema = new Schema({
-    name: { type: String, required: true },
-    description: { type: String, required: true },
-    imageUrl: { type: String, required: true },
+    name: { type: String, required: true, minlength: [3, 'Name must be at least 3 characters long'] },
+    description: { type: String, required: true, minlength: [10, 'Description must be at least 10 characters long'] },
+    imageUrl: {
+        type: String, required: true, validate: {
+            validator: (value) => URL_REGEX.test(value),
+            message: (props) => {
+                return `${props.value} is not a valid image URL`;
+            }
+        }
+    },
     cubes: { type: [Types.ObjectId], default: [], ref: 'Cube' }
 });
 
